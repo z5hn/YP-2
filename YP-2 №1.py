f0cbdfae27fd63ea21f408e3b23335c8338f9517
z5hn <belsis33@mail.ru>
2025-04-14 18:35:37 +0700

    second commit

--- YP-2 №1.py
+++ YP-2 №1.py
@@ -1,4 +1,11 @@
-"123123123123123123123"
+def count_jewels(J, S):
+    jewels = set(J)
+    count = 0
+    for char in S:
+        if char in jewels:
+            count += 1
+    return count
 
-
-
+J = input("Драгоценности:").strip()
+S = input("Камни:").strip()
+print(count_jewels(J, S))
